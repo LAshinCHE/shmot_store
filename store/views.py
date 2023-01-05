1ee0d780f36feea2335ba2ee74266ec668430504
LAshinCHE <egor.cherevichin1337@gmail.com>
2023-01-06 02:01:13 +0300

    add template

--- store/views.py
+++ store/views.py
@@ -1,1 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import  loader
+
+
+def index(request):
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
