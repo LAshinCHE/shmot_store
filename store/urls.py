fb7c0b542f1e01d5d2e97eef02afb030bc1078ad
Kirill Medvedev <41060824+Unknown-reader@users.noreply.github.com>
2023-01-16 22:26:23 +0300

    Add files via upload

--- store/urls.py
+++ store/urls.py
@@ -1,2 +1,8 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+
+from main import views
+
+urlpatterns = [
+    path('add-item-to-cart/<int:pk>', views.add_item_to_cart, name='add_item_to_cart'),
+]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,33 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import  loader
 
+from django.urls import reverse_lazy
+from django.views.generic import ListView
 
-def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+from store.forms import AddQuantityForm
+from store.models import Order, Product
+
+
+
+
+
+@login_required(login_url=reverse_lazy('login'))
+def add_item_to_cart(request, pk):
+    if request.method == 'POST':
+        quantity_form = AddQuantityForm(request.POST)
+        if quantity_form.is_valid():
+            quantity = quantity_form.cleaned_data['quantity']
+            if quantity:
+                cart = Order.get_cart(request.user)
+
+                product = get_object_or_404(Product, pk=pk)
+                cart.orderitem_set.create(product=product,
+                                          quantity=quantity,
+                                          price=product.price)
+                cart.save()
+                return redirect('cart_view')
+        else:
+            pass
+    return redirect('shop')
